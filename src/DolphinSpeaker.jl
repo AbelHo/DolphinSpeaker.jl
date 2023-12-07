29d69c2c9164a3d90a5541efc10106cd613e1fea
AbelHo <21101710+AbelHo@users.noreply.github.com>
2023-12-07 08:10:58 +0800

    feat: export showall()

--- src/DolphinSpeaker.jl
+++ src/DolphinSpeaker.jl
@@ -1,5 +1,8 @@
 module DolphinSpeaker
 @info "version v2023-12-03T19:01:00 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-"
+    include("utils.jl")
+    export showall
+    
     include("audio.jl")
     export mat2flac, mat2wav, mat2flac_check
 
