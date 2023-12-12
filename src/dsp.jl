500f7be57ceb32dd875406abb897211080d1f1f7
AbelHo <21101710+AbelHo@users.noreply.github.com>
2023-12-13 01:50:20 +0800

    feat(perf(dsp.jl)): custom threaded mapslices2

--- src/dsp.jl
+++ src/dsp.jl
@@ -13,7 +13,74 @@ function filter_simple(data, band_pass; fs=1, butterworth_size=butterworth_size)
         end
 
         filter_weight = digitalfilter(filter_type, Butterworth(butterworth_size))
-        data_filt = mapslices( x -> filtfilt( filter_weight, x), data, dims=1)
+        # data_filt = mapslices( x -> filtfilt( filter_weight, x), data, dims=1)
+        data_filt = mapslices2( x -> filtfilt( filter_weight, x), data)
     end
     return data_filt
+end
+
+function extrema_index(arr)
+    min_val, min_idx = arr[1], 1
+    max_val, max_idx = arr[1], 1
+
+    for i in 2:length(arr)
+        if arr[i] < min_val
+            min_val, min_idx = arr[i], i
+        elseif arr[i] > max_val
+            max_val, max_idx = arr[i], i
+        end
+    end
+
+    return (min_val, max_val, min_idx, max_idx)
+end
+
+
+function mapslices2(func, arr::Array{T, 2} where T)
+    # extrema_indices = Array{Tuple{T, Int, T, Int}, 1}(undef, size(arr, 2))
+    extrema_indices = Array{Any}(undef,size(arr, 2))
+    # lock = ReentrantLock()
+
+    Threads.@threads for j in 1:size(arr, 2)
+        # result = func(@view(arr[:,j]))
+        # lock(extrema_indices) do
+        #     extrema_indices[j] = result
+        # end
+        # min_val, min_idx = arr[1, j], 1
+        # max_val, max_idx = arr[1, j], 1
+
+        # for i in 2:size(arr, 1)
+        #     if arr[i, j] < min_val
+        #         min_val, min_idx = arr[i, j], i
+        #     elseif arr[i, j] > max_val
+        #         max_val, max_idx = arr[i, j], i
+        #     end
+        # end
+
+        extrema_indices[j] = func(@view(arr[:,j])) #(min_val, min_idx, max_val, max_idx)
+    end
+
+    return hcat(extrema_indices...)
+end
+
+
+function extrema_and_indices(arr::Array{T, 2} where T)
+    # extrema_indices = Array{Tuple{T, Int, T, Int}, 1}(undef, size(arr, 2))
+    extrema_indices = Array{Tuple}(undef,size(arr, 2))
+
+    Threads.@threads for j in 1:size(arr, 2)
+        min_val, min_idx = arr[1, j], 1
+        max_val, max_idx = arr[1, j], 1
+
+        for i in 2:size(arr, 1)
+            if arr[i, j] < min_val
+                min_val, min_idx = arr[i, j], i
+            elseif arr[i, j] > max_val
+                max_val, max_idx = arr[i, j], i
+            end
+        end
+
+        extrema_indices[j] = (min_val, max_val, min_idx, max_idx)
+    end
+
+    return extrema_indices
 end
