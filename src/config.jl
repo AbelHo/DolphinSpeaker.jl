45a73f3a3080fe05e42b08f91316dccbda4911c3
AbelHo <21101710+AbelHo@users.noreply.github.com>
2024-01-05 06:56:25 +0800

    refactor(config): change setting by using function call instead

--- src/config.jl
+++ src/config.jl
@@ -34,14 +34,57 @@ threshold_boat = 8e-6
 band_pass_boat = [1 500]
 
 
-if device_name=="aspod2"
-    band_pass = [5000, 24000]; winlen=0.01s;
-    threshold_impulsive = .005*32767#aspod2 .003#calf_hk #0.01 # 0.1#pinger/clickler #0.2
-    threshold_tonal = -20#aspod2 -110#calf_hk 
-    rx = 0.14722/sqrt(3) .* exp.(im.* deg2rad.([-30 90 -150]) ) #aspod 2
-    imsize=(2160, 3840)
-    fov_angle=[62.61721188568244,35.793211268714096,71.6855447884958]
-elseif device_name=="calf_hk"
+function set_device__hk_clicker()
+    global impulsive_band_pass = [1000, Inf] #fs/2*.98]
+    global threshold_impulsive = nothing# .003#calf_hk
+    global impulsive_autothreshold_median_ratio = 12
+
+    global threshold_tonal = nothing #-110#calf_hk -15#aspod2
+    global click_train_minlen = 2#calf 7 #5 20
+    global click_train_check_interval = 8#calf 1 #.01
+    global window_impulsive = -300:700 #-50:100
+    global dist_impulsive = 15000# 15000#pinger
+
+    global rx = 0.4/sqrt(3) .* exp.(im.* deg2rad.([150 -90 30]) ) # calf_hk
+    global imsize=(2160, 3840)
+    global fov_angle=[54,34,0]
+end
+
+function set_device__calf_hk()
+    global impulsive_band_pass = [1000, Inf] #fs/2*.98]
+    global threshold_impulsive = nothing #.1# .003#calf_hk
+    global impulsive_autothreshold_median_ratio = 4;#10;
+    # global # dist_impulsive = 400#800# 15000#pinger
+
+    global click_train_minlen = 5#calf 7 #5 20  #[n+1]clicks within (click_train_check_interval)seconds
+    global click_train_check_interval = 0.1#calf 1 #.01  #seconds has at least (click_train_minlen) clicks
+
+    global window_impulsive = -300:700; #-50:200; #-5000:10000 #-50:300
+
+    global threshold_tonal = nothing #-110#calf_hk -15#aspod2
+    global tonal_band_pass = [2500, 24000]
+
+    global rx = 0.4/sqrt(3) .* exp.(im.* deg2rad.([150 -90 30]) ) # calf_hk
+    global imsize=(2160, 3840)
+    global fov_angle=[54,34,0]
+
+    global threshold_boat = Inf #1e3
+    global band_pass_boat = [1 500]
+    # global default_getTDOA_func = get_tdoa_raw_MaxEnergyRefChannel
+
+
+end
+
+function set_device__aspod2()
+    global band_pass = [5000, 24000]; winlen=0.01s;
+    global threshold_impulsive = .005*32767#aspod2 .003#calf_hk #0.01 # 0.1#pinger/clickler #0.2
+    global threshold_tonal = -20#aspod2 -110#calf_hk 
+    global rx = 0.14722/sqrt(3) .* exp.(im.* deg2rad.([-30 90 -150]) ) #aspod 2
+    global imsize=(2160, 3840)
+    global fov_angle=[62.61721188568244,35.793211268714096,71.6855447884958]
+end
+
+#~ default
     impulsive_band_pass = [1000, Inf] #fs/2*.98]
     threshold_impulsive = nothing #.1# .003#calf_hk
     impulsive_autothreshold_median_ratio = 4;#10;
@@ -62,6 +105,37 @@ elseif device_name=="calf_hk"
     threshold_boat = Inf #1e3
     band_pass_boat = [1 500]
 
+
+
+if device_name=="aspod2"
+    band_pass = [5000, 24000]; winlen=0.01s;
+    threshold_impulsive = .005*32767#aspod2 .003#calf_hk #0.01 # 0.1#pinger/clickler #0.2
+    threshold_tonal = -20#aspod2 -110#calf_hk 
+    rx = 0.14722/sqrt(3) .* exp.(im.* deg2rad.([-30 90 -150]) ) #aspod 2
+    imsize=(2160, 3840)
+    fov_angle=[62.61721188568244,35.793211268714096,71.6855447884958]
+elseif device_name=="calf_hk"
+    set_device__calf_hk()
+    # impulsive_band_pass = [1000, Inf] #fs/2*.98]
+    # threshold_impulsive = nothing #.1# .003#calf_hk
+    # impulsive_autothreshold_median_ratio = 4;#10;
+    # # dist_impulsive = 400#800# 15000#pinger
+
+    # click_train_minlen = 5#calf 7 #5 20  #[n+1]clicks within (click_train_check_interval)seconds
+    # click_train_check_interval = 0.1#calf 1 #.01  #seconds has at least (click_train_minlen) clicks
+
+    # window_impulsive = -300:700; #-50:200; #-5000:10000 #-50:300
+
+    # threshold_tonal = nothing #-110#calf_hk -15#aspod2
+    # tonal_band_pass = [2500, 24000]
+
+    # rx = 0.4/sqrt(3) .* exp.(im.* deg2rad.([150 -90 30]) ) # calf_hk
+    # imsize=(2160, 3840)
+    # fov_angle=[54,34,0]
+
+    # threshold_boat = Inf #1e3
+    # band_pass_boat = [1 500]
+
 elseif device_name=="punnet"
     # threshold_impulsive = .005*32767#aspod2 .003#calf_hk #0.01 # 0.1#pinger/clickler #0.2
     threshold_tonal = -20#aspod2 -110#calf_hk 
@@ -71,17 +145,18 @@ elseif device_name=="punnet"
     # click_train_check_interval = .2;click_train_minlen = 4;threshold_impulsive = nothing
     rx = 0.5/sqrt(3) .* exp.(im.* deg2rad.([90 -30 210]) ) #0.5/sqrt(3) .* exp.(im.* deg2rad.([-30 90 -150]) )
 elseif device_name=="calf_hk_clicker"
-    impulsive_band_pass = [1000, Inf] #fs/2*.98]
-    threshold_impulsive = .1# .003#calf_hk
-    threshold_tonal = nothing #-110#calf_hk -15#aspod2
-    click_train_minlen = 2#calf 7 #5 20
-    click_train_check_interval = 5#calf 1 #.01
-    window_impulsive = -300:700 #-50:100
-    dist_impulsive = 15000# 15000#pinger
+    set_device__hk_clicker()
+    # impulsive_band_pass = [1000, Inf] #fs/2*.98]
+    # threshold_impulsive = .1# .003#calf_hk
+    # threshold_tonal = nothing #-110#calf_hk -15#aspod2
+    # click_train_minlen = 2#calf 7 #5 20
+    # click_train_check_interval = 5#calf 1 #.01
+    # window_impulsive = -300:700 #-50:100
+    # dist_impulsive = 15000# 15000#pinger
 
-    rx = 0.4/sqrt(3) .* exp.(im.* deg2rad.([150 -90 30]) ) # calf_hk
-    imsize=(2160, 3840)
-    fov_angle=[54,34,0]
+    # rx = 0.4/sqrt(3) .* exp.(im.* deg2rad.([150 -90 30]) ) # calf_hk
+    # imsize=(2160, 3840)
+    # fov_angle=[54,34,0]
 end
 
 rx_vect = [real(rx); imag(rx); zeros(1,3)]
@@ -97,3 +172,4 @@ rx_vect = [real(rx); imag(rx); zeros(1,3)]
 #     folname = "/run/user/1000/gvfs/smb-share:server=10.246.128.21,share=data/Aspod/Aspod4/Bahamas_2022" #"/media/sousa/Space/data/aspod2/Bahamas_2022" #"/media/sousa/My Passport/Bahamas_2022"
 # end
 
+set_device__calf_hk()
