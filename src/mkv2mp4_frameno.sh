32a83d3197af67686e17c4bca38652432e549f4a
AbelHo <21101710+AbelHo@users.noreply.github.com>
2024-06-10 03:41:12 +0800

    feat: mkv2mp4 add frame number overaly top left

---
+++ src/mkv2mp4_frameno.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+src_dir="$1"
+dst_dir="$2"
+
+find "$src_dir" -name "*.mkv" -type f | while read -r file
+do
+    # echo $file
+    rel_path="${file#$src_dir}"
+    dst_file="$dst_dir/${rel_path%.mkv}.mp4"
+    mkdir -p "$(dirname "$dst_file")"
+    ffmpeg -i "$file" -filter_complex "drawtext=text='%{n}': x=10: y=35: fontsize=48: fontcolor=red" "$dst_file"
+done
