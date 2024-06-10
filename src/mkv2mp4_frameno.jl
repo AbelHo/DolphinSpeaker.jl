758a1f7f07d6c51aa356b667dd768bc95fce5ba7
AbelHo <21101710+AbelHo@users.noreply.github.com>
2024-06-10 11:16:06 +0800

    fix(mkv2mp4)

--- src/mkv2mp4_frameno.jl
+++ src/mkv2mp4_frameno.jl
@@ -6,9 +6,9 @@ out_dir = "/run/user/1000/gvfs/smb-share:server=10.246.128.21,share=results/Conc
 
 function convert_vid(src::String, dst::String)
     if endswith(src, ".mkv")
-        dst = joinpath(dst, replace(basename(src), ".mkv" => ".mp4"))
-        @info (src, dst)
-        run(`ffmpeg -i $src -filter_complex "drawtext=text='%{n}': x=10: y=35: fontsize=48: fontcolor=red" $dst -loglevel error`)
+        dst2 = joinpath(dst, replace(basename(src), ".mkv" => ".mp4"))
+        @info (src, dst2)
+        run(`ffmpeg -i $src -filter_complex "drawtext=text='%{n}': x=10: y=35: fontsize=48: fontcolor=red" $dst2 -loglevel error`)
     end
 end
 
