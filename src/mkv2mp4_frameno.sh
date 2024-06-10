75b49f65ea773f3b6b33ff1185d63259ca7e2948
AbelHo <21101710+AbelHo@users.noreply.github.com>
2024-06-10 09:28:03 +0800

    fix(mkv3mp4_frame): ""

--- src/mkv2mp4_frameno.sh
+++ src/mkv2mp4_frameno.sh
@@ -6,7 +6,7 @@ dst_dir="$2"
 find "$src_dir" -name "*.mkv" -type f | while read -r file
 do
     # echo $file
-    rel_path="${file#$src_dir}"
+    rel_path=${file#"$src_dir"}
     dst_file="$dst_dir/${rel_path%.mkv}.mp4"
     mkdir -p "$(dirname "$dst_file")"
     ffmpeg -i "$file" -filter_complex "drawtext=text='%{n}': x=10: y=35: fontsize=48: fontcolor=red" "$dst_file"
