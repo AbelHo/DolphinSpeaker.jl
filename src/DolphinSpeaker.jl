1d8aaf1c5a27fdab2996806353c868a99bfb98fe
AbelHo <21101710+AbelHo@users.noreply.github.com>
2024-07-15 12:08:11 +0800

    feat(DolphinSpeaker.jl): export process_files, multisync

--- src/DolphinSpeaker.jl
+++ src/DolphinSpeaker.jl
@@ -2,13 +2,13 @@ module DolphinSpeaker
     version = "2024-03-15T20:00"
     @info "version v$version =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-"
     include("utils.jl")
-    export showall, skiphiddenfiles
+    export showall, skiphiddenfiles, process_files
     
     include("audio.jl")
     export mat2flac, mat2wav, mat2flac_check, bin2flac, bin2flac_check
 
     include("synchronization.jl")
-    export findBlip_bothVidAudio, flac2signal
+    export findBlip_bothVidAudio, flac2signal, multisync
 
     include("run_example.jl")
     export process_one_set, process_folder, process_dir
