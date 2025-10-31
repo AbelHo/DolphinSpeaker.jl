401a9e3d3cc17ca414c197fec4518a9babbff587
AbelHo <21101710+AbelHo@users.noreply.github.com>
2025-10-31 23:54:55 +0800

    feat(media): add concat_media function for merging video files and add combine_vidau for audio normalization

--- src/media_info.jl
+++ src/media_info.jl
@@ -259,4 +259,92 @@ function get_ffmpeg_metadata(fname::AbstractString)
   return JSON.parse(output)
 end
 
+function concat_media(filelist::Vector{String}, outputfile_dir::String; flag_output_auto=true, flag_overwrite=false)
+	# create a temporary text file listing the input files
+	ext = splitext(filelist[1])[2]
+	if flag_output_auto
+		outputfile = joinpath(outputfile_dir, "combined__$(join(basename.(filelist), '_'))$ext")
+	else
+		outputfile = outputfile_dir
+	end
+	tmpfile = joinpath(dirname(outputfile), "temp_filelist.txt")
+	open(tmpfile, "w") do io
+		for f in filelist
+			write(io, "file '$f'\n")
+		end
+	end
+
+	overwrite = flag_overwrite ? " -y" : ""
+
+	# use the concat demuxer as input 0 and add the first file as input 1,
+	# then copy streams from input 0 and copy metadata from input 1
+	firstfile = filelist[1]
+	cmd = `ffmpeg -hide_banner -loglevel error -f concat -safe 0 -i $tmpfile -i $firstfile -c copy -map 0 -map_metadata 1 $outputfile`
+	# _cmd = `ffmpeg -hide_banner -loglevel error -f concat -safe 0 -i $tmpfile -i $firstfile -c copy -map 0 -map_metadata 1 -map_chapters 1 -movflags use_metadata_tags $outputfile`
+	
+	# run the command
+	try
+		@ffmpeg_env run(`$cmd$overwrite`)
+	catch err
+		@warn "Concatenation failed with Julia's FFMPEG, trying again with system call: $err"
+		run(`$cmd -y`)
+	finally
+		# delete the temporary file
+		# rm(tmpfile; force=true)
+	end
+	return outputfile
+end
+
+#~ combine video and audio
+function combine_vidau(newvidname, aufname_list; vidau_syncdiff=0, MERGE_VID_AU_DYNAMIC_NORM=false, rx_vect=rx_vect, flag_rm_oldfile=false, kwargs...) #TODO: swap audio channel according to rx_vect location to correspond Left, Right, Center
+    if aufname_list isa Array
+        if length(aufname_list)==1
+            aufname = aufname_list[1]
+        else
+            aufname = concat_media(aufname_list, dirname(newvidname))
+        end
+    end
+    
+    if MERGE_VID_AU_DYNAMIC_NORM
+        cmd = `$ffmpeg -i "$newvidname" -itsoffset $vidau_syncdiff -i "$aufname" -map 0:v -map 1:a -pix_fmt yuv420p -af loudnorm=I=-16:LRA=11:TP=-1.5 "$newvidname""_DYnormalized-audio.mp4"`
+        # println(`$ffmpeg -i "$newvidname" -itsoffset $vidau_syncdiff -i "$aufname" -map 0:v -map 1:a -pix_fmt yuv420p -af loudnorm=I=-16:LRA=11:TP=-1.5 -f matroska "$newvidname""_DYnormalized-audio.mkv"`)
+        # output = @ffmpeg_env run(`$ffmpeg -i "$newvidname" -itsoffset $vidau_syncdiff -i "$aufname" -map 0:v -map 1:a -pix_fmt yuv420p -af loudnorm=I=-16:LRA=11:TP=-1.5 "$newvidname""_DYnormalized-audio.mp4"`)
+        # run(`ffmpeg -i "$newvidname" -i "$aufname" -map 0:v -map 1:a -vcodec copy -af loudnorm=I=-16:LRA=11:TP=-1.5 -f matroska "$newvidname""_normalized-audio.mkv"`)
+    else
+        # Get max volume and normalize
+        tempfile = tempname()
+        # read(pipeline(`ffmpeg -i $aufname -filter:a volumedetect -f null /dev/null`; stderr = tempfile))
+        @ffmpeg_env read(pipeline(`ffmpeg -i $aufname -af astats=metadata=1 -f null /dev/null`; stderr = tempfile))
+
+        ss = read(tempfile, String)
+        rm(tempfile)
+        m = eachmatch(r"Channel: (\d+)", ss)
+        channel_list = [parse(Int, m.captures[1]) for m in m]
+        m = eachmatch(r"Peak level dB: (.*)", ss)
+        peak_db_list = [parse(Float64, m.captures[1]) for m in m]
+        norm_gain = -maximum(peak_db_list[get_relevant_channels(rx_vect)])
+
+        # m = match(r"max_volume: (.*) dB", ss)
+        # max_volume = m !== nothing ? parse(Float64, m.captures[1]) : nothing
+        # norm_gain = -max_volume
+        cmd = `$ffmpeg -i "$newvidname" -itsoffset $vidau_syncdiff -i "$aufname" -map 0:v -map 1:a -pix_fmt yuv420p -af "volume=$(norm_gain)dB" "$newvidname""_normalized-audio.mp4"`
+        
+    end
+    println(cmd)
+    try
+        output = @ffmpeg_env run(cmd)
+    catch err
+        @warn "Combining video and audio failed with Julia's FFMPEG, trying again with system call"
+        output = run(`$cmd -y`)
+    end
+
+    isfile("$newvidname"*"_normalized-audio.mp4") && flag_rm_oldfile && rm(newvidname) # delete video without audio
+    
+    aufname_list isa Array && length(aufname_list) >1 && rm(aufname) # delete concatenated audio file
+
+    return "$newvidname"*"_normalized-audio.mp4"
+    # if aufname_old isa String; rm(aufname); end
+end
+
+
 @info "LOADED!\tmedia_info"
