0df2873dc6890fa9e27888089aceef92aba2781c
AbelHo <21101710+AbelHo@users.noreply.github.com>
2025-10-31 23:08:23 +0800

    feat(audio): add fname2dt_goprolocal function for extracting creation time from gopro metadata

--- src/audio.jl
+++ src/audio.jl
@@ -129,6 +129,7 @@ fname2dt_date_time(aufname; skip_front=0) = DateTime(basename(aufname)[(1:15) .+
 fname2dt_soundtrap(aufname) = DateTime("20"*basename(aufname)[findfirst(".", basename(aufname))[1] .+ (1:12)], dateformat"yyyymmddHHMMSS") #DateTime("20"*basename(aufname)[6:17], dateformat"yyyymmddHHMMSS")
 fname2dt_ls1 = fname2dt_date_time
 fname2dt_zoomf6(aufname) = DateTime(get_ffmpeg_metadata(aufname)["format"]["tags"]["date"] *"T"* get_ffmpeg_metadata(aufname)["format"]["tags"]["creation_time"]) #, dateformat"yyyy-mm-ddTHH:MM:SS")
+fname2dt_goprolocal(vidfname) = DateTime(get_ffmpeg_metadata(vidfname)["format"]["tags"]["creation_time"][1:end-5])
 
 function writeWAV(data, fpath; Fs=1)
     nbits = parse(Int64, string(eltype(data))[end-1:end])
